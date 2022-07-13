fced9b8b4d6e211b91e7ed1dde398645d78e57bc
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-07-13 14:39:05 -0400

    Update index.js

--- lesson_1/index.js
+++ lesson_1/index.js
@@ -1,1 +1,7 @@
 ReactDOM.render(<h1>Hello, Everyone!</h1>, document.getElementById("root"));
+
+/*
+Render a <ul> with 2+ <li> inside
+*/
+
+ReactDOM.render(<ul><li>Pancakes</li><li>Waffles</li></ul>, document.getElementById("root"));
